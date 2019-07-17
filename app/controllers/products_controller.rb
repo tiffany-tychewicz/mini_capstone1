ef44d350dd8b74c12b2e8253d1cab58f82de879a
Siva <carah.tyche@gmail.com>
2019-07-16 19:49:56 -0500

    Working on update action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,4 +32,15 @@ class ProductsController < ApplicationController
     render "edit.html.erb"
   end
 
+  d  def update
+    @products = Product.find_by(id: params[:id])
+    @products.name = params[:input_name
+    @products.price = params[:input_price]
+    @products.image_url = params[:input_image_url]
+    @products.description = params[:input_description]
+    @products.user_id = params[:user_id]
+    @products.save
+    redirect_to "/products"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
   get "/products/:id/edit" => "products#edit"
+  patch "/products/:id" => "products#update"
 
 
   namespace :api do
